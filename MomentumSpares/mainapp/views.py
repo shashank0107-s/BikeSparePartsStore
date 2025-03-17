ed667a79f6beb0fb6e6a1dc559e01fa55d6d4eb6
shashank0107-s <shashank0107.s@gmail.com>
2025-03-17 20:05:53 +0530

    added mainapp,templates

---
+++ MomentumSpares/mainapp/views.py
@@ -0,0 +1,9 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
